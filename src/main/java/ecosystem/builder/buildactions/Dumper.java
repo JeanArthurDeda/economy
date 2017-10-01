c93f708b3b595e397884e074ebe57a686f712e12
Iean-Arthur Deda <JeanArthurDeda@yahoo.com>
2017-10-01 22:47:44 +0300

    Improve the voronoi drawing and SpiralIterator

--- src/main/java/ecosystem/builder/buildactions/Dumper.java
+++ src/main/java/ecosystem/builder/buildactions/Dumper.java
@@ -88,50 +88,102 @@ public class Dumper extends BuildAction {
     }
 
     private void drawVoronoi(BufferedImage image, Ecosystem ecosystem, Class<? extends Entity> clasa, int color) {
-        QuadPartitionedEntities partioner = ecosystem.getPartitioner(clasa);
-
+        long time = System.currentTimeMillis();
         LocationStack stack = new LocationStack(3);
-        double texelSize = 0.5 * Math.sqrt (8.0) / Math.sqrt (mWidth * mWidth + mHeight * mHeight);
+        double texelSize = 0.5 * Location.WORLD_DIAGONAL_SIZE / Math.sqrt (mWidth * mWidth + mHeight * mHeight);
 
+        // ===================================
+        // create the spiral iterator receiver
+        // ===================================
+        QuadPartitionedEntities partioner = ecosystem.getPartitioner(clasa);
+        SeriList<PartitionQuad> quads = partioner.getQuads();
+        class VoronoiMinGatherer extends SpiralIterator.Receiver{
+            protected double mLocationX;
+            protected double mLocationY;
+
+            protected double mMin1 = Double.MAX_VALUE;
+            protected Location mLoc1 = null;
+            protected double mMin2 = Double.MAX_VALUE;
+            protected Location mLoc2 = null;
+            protected boolean mQuadProcessed;
+
+            public void setLocation (double locationX, double locationY){
+                mLocationX = locationX;
+                mLocationY = locationY;
+            }
+
+            public Location getLoc1() {
+                return mLoc1;
+            }
+
+            public Location getLoc2() {
+                return mLoc2;
+            }
+
+            @Override
+            public void onStart(int startX, int startY) {
+                mMin1 = Double.MAX_VALUE;
+                mLoc1 = null;
+                mMin2 = Double.MAX_VALUE;
+                mLoc2 = null;
+                mQuadProcessed = false;
+            }
+
+            @Override
+            public boolean onStartSquare(int x, int y, int startX, int startY) {
+                if (!mQuadProcessed)
+                    return false;
+                mQuadProcessed = false;
+                return true;
+            }
+
+            @Override
+            public boolean onValidValue(int x, int y, int startX, int startY) {
+                int index = partioner.getNumQuads() * y + x;
+                PartitionQuad quad = quads.get(index);
+                double distToQuad = quad.getBound().dist(mLocationX, mLocationY);
+                if (mMin2 < distToQuad)
+                    return true;
+                mQuadProcessed = true;
+
+                SeriEntities entities = quad.getEntities();
+                for (Entity entity : entities) {
+                    Location location = entity.getLocation();
+                    double d = location.dist(mLocationX, mLocationY);
+                    if (d < mMin1) {
+                        if (mMin1 < mMin2){
+                            mMin2 = mMin1;
+                            mLoc2 = mLoc1;
+                        }
+                        mMin1 = d;
+                        mLoc1 = entity.getLocation();
+                    } else if (d < mMin2){
+                        mMin2 = d;
+                        mLoc2 = entity.getLocation();
+                    }
+                }
+                return true;
+            }
+        }
+        SpiralIterator spiralIterator = new SpiralIterator();
+        VoronoiMinGatherer voronoiMinGatherer = new VoronoiMinGatherer();
+
+        // =======
+        // Voronoi
+        // =======
         for (int y = 0; y < mHeight; ++y)
             for (int x  = 0; x < mWidth; ++x){
                 double locationX = ((double)x / (double)mWidth) * 2.0 - 1.0;
                 double locationY = ((double)y / (double)mHeight) * 2.0 - 1.0;
-
-                double min1 = Double.MAX_VALUE;
-                Location loc1 = null;
-                double min2 = Double.MAX_VALUE;
-                Location loc2 = null;
-
-                SeriList<PartitionQuad> quads = partioner.getQuads();
                 int quadX = partioner.getQuadX(locationX);
                 int quadY = partioner.getQuadY(locationY);
-                SpiralIterator spiralIterator = new SpiralIterator(quadX, quadY, partioner.getNumQuads(), partioner.getNumQuads());
 
-                for (int index = spiralIterator.get(); index != -1; index = spiralIterator.get()){
-                    PartitionQuad quad = quads.get(index);
-                    double distToQuad = quad.getBound().dist(locationX, locationY);
-                    if (min2 < distToQuad)
-                        continue;
 
-                    SeriEntities entities = quad.getEntities();
-                    for (int i = 0; i < entities.size(); i++) {
-                        Entity entity = entities.get(i);
-                        Location location = entity.getLocation();
-                        double d = location.dist(locationX, locationY);
-                        if (d < min1) {
-                            if (min1 < min2){
-                                min2 = min1;
-                                loc2 = loc1;
-                            }
-                            min1 = d;
-                            loc1 = entity.getLocation();
-                        } else if (d < min2){
-                            min2 = d;
-                            loc2 = entity.getLocation();
-                        }
-                    }
-                }
+                voronoiMinGatherer.setLocation(locationX, locationY);
+                spiralIterator.iterate(quadX, quadY, partioner.getNumQuads(), partioner.getNumQuads(), voronoiMinGatherer);
+
+                Location loc1 = voronoiMinGatherer.getLoc1();
+                Location loc2 = voronoiMinGatherer.getLoc2();
 
                 // compute the middle point between the voronoi cells
                 stack.reset();
@@ -151,6 +203,8 @@ public class Dumper extends BuildAction {
                 if (d < texelSize)
                     image.setRGB(x, y, color);
             }
+        time = System.currentTimeMillis() - time;
+        System.out.println(String.format("DrawVoronoi %d", time));
     }
 
     protected void drawPoints(BufferedImage image, Ecosystem ecosystem, Class<? extends Entity> clasa, int color) {
@@ -162,7 +216,7 @@ public class Dumper extends BuildAction {
             Location location = entity.getLocation();
             double x = (location.getX() * 0.5 + 0.5) * mWidth;
             double y = (location.getY() * 0.5 + 0.5) * mHeight;
-            render.fill(new Rectangle2D.Double(x - 1.0, y - 1.0, 2.0, 2.0));
+            render.fill(new Rectangle2D.Double(x - 0.5, y - 0.5, 0.5, 0.5));
         }
     }
 }
